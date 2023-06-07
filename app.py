9404c6306d64edd32d560a86e5f138db732b2482
fadlanamin <fathelan@gmail.com>
2023-06-07 19:02:14 +0700

    "Ini add list"

--- app.py
+++ app.py
@@ -17,5 +17,27 @@ def halaman_dua():
 def welcoming_user(nama):
     return f"Halo, Selamat Datang di Halaman {nama}"
 
+
+#ini kalo mau buat kalimat nya lebih dari satu 
+@apps.route("/halaman3", methods=["GET"])
+
+def halaman_tiga():
+
+    return """
+
+<h1>Halo, Selamat Datang di Halaman Tiga</h1>
+
+<p>Ini adalah paragraf</p>
+
+<ul>
+
+    <li>Ini adalah list 1</li>
+
+    <li>Ini adalah list 2</li>
+
+<ul>
+
+"""
+
 if __name__ == "__main__":
     apps.run()
