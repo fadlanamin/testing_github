0b80f1149d2201b0c718db09a8f110dc6d660b82
fadlanamin <fathelan@gmail.com>
2023-06-12 01:12:54 +0700

    'data reset #2'

--- app.py
+++ app.py
@@ -1,69 +1,100 @@
-#Create API with Flask
-
-from flask import Flask, jsonify, render_template
-
-
-apps = Flask(__name__)
-
-#----ini fungsi api yang get----
-
-@apps.route("/", methods=["GET"])
-def halaman_utama():
-    return "Halo, Selamat Datang di Halaman utama"
-
-@apps.route("/halaman2", methods=["GET"])
-def halaman_dua():
-    return "Halo, Selamat Datang di Halaman kedua"
-
-@apps.route("/<string:nama>", methods=["GET"])
-def welcoming_user(nama):
-    return f"Halo, Selamat Datang di Halaman {nama}"
-
-
-#ini kalo mau buat kalimat nya lebih dari satu 
-@apps.route("/halaman3", methods=["GET"])
-
-def halaman_tiga():
-
-    return """
-
-<h1>Halo, Selamat Datang di Halaman Tiga</h1>
-
-<p>Ini adalah paragraf</p>
-
-<ul>
-
-    <li>Ini adalah list 1</li>
-
-    <li>Ini adalah list 2</li>
-
-<ul>
-
+""" 
+Flask API Application
 """
+from flask import Flask, jsonify, request
+import pandas as pd
+from flasgger import Swagger, swag_from, LazyString, LazyJSONEncoder
+from import_data import (
+    create_connection, insert_dictionary_to_db, 
+    insert_result_to_db, show_cleansing_result,
+    insert_upload_result_to_db
+)
+from cleansing_function import text_cleansing, cleansing_files
 
-#-- ini fungsi api yang post---
+# Prevent sorting keys in JSON response
+import flask
+flask.json.provider.DefaultJSONProvider.sort_keys = False
 
-@apps.route("/penjumlahan", methods=["POST"])
+# Set Up Database
+db_connection = create_connection()
+insert_dictionary_to_db(db_connection)
+db_connection.close()
 
-def penjumlahan():
+# Initialize flask application
+app = Flask(__name__)
+# Assign LazyJSONEncoder to app.json_encoder for swagger UI
+app.json_encoder = LazyJSONEncoder
+# Create Swagger Config & Swagger template
+swagger_template = {
+    "info": {
+        "title": LazyString(lambda: "Text Cleansing API"),
+        "version": LazyString(lambda: "1.0.0"),
+        "description": LazyString(lambda: "Dokumentasi API untuk membersihkan text")
+    },
+    "host": LazyString(lambda: request.host)
+}
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": 'docs',
+            "route": '/docs.json',
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/docs/"
+}
+# Initialize Swagger from swagger template & config
+swagger = Swagger(app, template=swagger_template, config=swagger_config)
 
-    """ 
+# Homepage
+@swag_from('docs/home.yml', methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    welcome_msg = {
+        "version": "1.0.0",
+        "message": "Welcome to Flask API",
+        "author": "Benedictus Aryo"
+    }
+    return jsonify(welcome_msg)
 
-    To request using curl:
+# Show cleansing result
+@swag_from('docs/show_cleansing_result.yml', methods=['GET'])
+@app.route('/show_cleansing_result', methods=['GET'])
+def show_cleansing_result_api():
+    db_connection = create_connection()
+    cleansing_result = show_cleansing_result(db_connection)
+    return jsonify(cleansing_result)
 
-    curl -X POST -H "Content-Type: application/json" -d '{"angka1": 1, "angka2": 2}' http://localhost:5000/penjumlahan
+# Cleansing text using form
+@swag_from('docs/cleansing_form.yml', methods=['POST'])
+@app.route('/cleansing_form', methods=['POST'])
+def cleansing_form():
+    # Get text from input user
+    raw_text = request.form["raw_text"]
+    # Cleansing text
+    clean_text = text_cleansing(raw_text)
+    result_response = {"raw_text": raw_text, "clean_text": clean_text}
+    # Insert result to database
+    db_connection = create_connection()
+    insert_result_to_db(db_connection, raw_text, clean_text)
+    return jsonify(result_response)
 
-    """
+# Cleansing text using csv upload
+@swag_from('docs/cleansing_upload.yml', methods=['POST'])
+@app.route('/cleansing_upload', methods=['POST'])
+def cleansing_upload():
+    # Get file from upload to dataframe
+    uploaded_file = request.files['upload_file']
+    # Read csv file to dataframe then cleansing
+    df_cleansing = cleansing_files(uploaded_file)
+    # Upload result to database
+    db_connection = create_connection()
+    insert_upload_result_to_db(db_connection, df_cleansing)
+    print("Upload result to database success!")
+    result_response = df_cleansing.T.to_dict()
+    return jsonify(result_response)
 
-    data = request.json
-
-    hasil = int(data["angka1"]) + int(data["angka2"])
-
-    return {"hasil": hasil}
-
-if __name__ == "__main__":
-    apps.run() 
-    # apps.run(port=8000, debug=True)
-    #ini port atau linknya diubah jadi 8000
-    #ini debug=True , setiap kita save akan dijalanin lagi atau diupdate
-    #not recommended buat beginner
+if __name__ == '__main__':
+    app.run()
